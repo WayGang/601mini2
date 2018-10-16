0d1d927d8ce6ef2587c5ce9da26a911b46772096
Gang Wei <43114190+WayGang@users.noreply.github.com>
2018-10-16 00:21:48 -0400

    just start

---
+++ frame.py
@@ -0,0 +1,43 @@
+# Copyright 2018 Gang Wei wg0502@bu.edu
+
+# framework
+# define functions
+
+
+img_path =
+
+def resize(): # resize images
+    '''
+    image = image.resize
+    '''
+
+def load():
+    '''
+    load the data list
+    load the images
+    to the net
+    '''
+
+def xxxnet():#build up the net
+    '''
+    may use tensorflow
+    may use karas
+    may use tflearn
+    '''
+
+def train():
+    '''
+    model,data,images
+    '''
+
+def test():
+    '''
+    model,images
+    '''
+
+
+if __name__ == '__main__':
+    accuracy = error_num/image_num
+    print ("The accuracy is:",accuracy)
+    # train()
+    # test()
